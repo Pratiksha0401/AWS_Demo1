ddf5a283be02b15d0a20375f82015af394165663
Pratiksha0401 <pratiksha411998@gmail.com>
2022-01-21 16:05:33 +0530

    Initial Commit

---
+++ src/main/java/com/example/demo/Hello.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+
+    @GetMapping("/greet/{name}")
+    public String greet(@PathVariable String name){
+        return "Hello "+ name;
+    }
+
+}
